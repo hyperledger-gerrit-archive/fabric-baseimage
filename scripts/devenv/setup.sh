8065574d77e371a52c048470128734497999b8af
Jason Yellick <jyellick@us.ibm.com>
2017-01-04 14:35:59 -0500

    [FAB-1508] Add baseimage behave python deps
    https://jira.hyperledger.org/browse/FAB-1508
    
    The behave tests now require the sha3 and OpenSSL packages.  This
    changeset adds them as dependencies to the devenv setup scripts as
    well as the documentation.
    
    A separate changeset for the fabric is pending.
    
    Change-Id: Ic639ba97dd7361b53b887a6511f2cc9ee6086478
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- scripts/devenv/setup.sh
+++ scripts/devenv/setup.sh
@@ -29,7 +29,7 @@ pip install behave
 pip install nose
 
 # updater-server, update-engine, and update-service-common dependencies (for running locally)
-pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3
+pip install -I flask==0.10.1 python-dateutil==2.2 pytz==2014.3 pyyaml==3.10 couchdb==1.0 flask-cors==2.0.1 requests==2.4.3 pyOpenSSL==16.2.0 sha3==0.2.1
 
 # Python grpc package for behave tests
 # Required to update six for grpcio
